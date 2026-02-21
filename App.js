d04e52331a71ddaa359d05831c33561864d7ff28
neeldarji12 <neeldarji1431@gmail.com>
2026-02-21 09:47:25 +0530

    Add FirebaseStorage component to App

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
+import React from "react";
+import FirebaseStorage from "./FirebaseStorage";
 
+function App() {
+  return (
+    <div>
+      <FirebaseStorage />
+    </div>
+  );
+}
+
+export default App;
